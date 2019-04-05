cda410218797491d6f2680c2725b95a831984717
isadoraBnunes <isadora.nunes@novaandradina.org>
2019-04-04 23:21:25 -0300

    atualizando de novo

--- js/cesar.js
+++ js/cesar.js
@@ -1,20 +1,21 @@
 const array = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
 document.getElementById("codificar").addEventListener('click', () => {
-  var chaveCodificar = document.querySelector("#chaveCodificar").value;
-  var textoCodificar = document.getElementById("textoCodificar").value.toUpperCase();
-  var resultado = "";
+  let chaveCodificar = document.querySelector("#chaveCodificar").value;
+  let textoCodificar = document.getElementById("textoCodificar").value.toLowerCase();
+  let resultado = "";
 
   for (var i = 0; i < textoCodificar.length; i++) {
-    var posicionamentoDaLetra = textoCodificar.charCodeAt(i)-64;
-    var deslocamentoDaLetra = (posicionamentoDaLetra + parseInt(chaveCodificar)) % 26;
+
+    let posicionamentoDaLetra = textoCodificar.charCodeAt(i)-64; //posição da letra no alfabeto
+    let deslocamentoDaLetra = (posicionamentoDaLetra + parseInt(chaveCodificar)) % 26; //deslocamento da letra para cifra de cesar e se mantem dentro das 26 letras
     resultado += array[deslocamentoDaLetra - 1];
   }
-  document.getElementById("resultado").innerHTML = resultado;
+   document.getElementById("resultado").innerHTML = resultado;
 });
 
 
-document.getElementById("Descodificar").addEventListener('click', () => {
+document.getElementById("Decodificar").addEventListener('click', () => {
   var chaveDecodificar = document.querySelector("#chaveDecodificar").value;
   var textoDecodificar = document.getElementById("textoDecodificar").value.toUpperCase();
   var resultado = "";
