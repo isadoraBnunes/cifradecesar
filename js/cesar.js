5dafb60c6a42e11a5d04cd3da4561bdbd7c2dd71
isadorabnunes <isadora.nunes@novaandradina.org>
2019-04-09 13:35:58 -0300

    nova atualização

--- js/cesar.js
+++ js/cesar.js
@@ -1,29 +1,36 @@
 const array = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
 document.getElementById("codificar").addEventListener('click', () => {
-  let chaveCodificar = document.querySelector("#chaveCodificar").value;
-  let textoCodificar = document.getElementById("textoCodificar").value.toUpperCase();
-  let resultado = "";
+  let chaveCodificar = parseInt(document.querySelector("#chaveCodificar").value);
+  let textoCodificar = document.getElementById("textoCodificar").value.toLowerCase();
+  let resultado = [];
 
-  for (var i = 0; i < textoCodificar.length; i++) {
-
-    var posicionamentoDaLetra = textoCodificar.charCodeAt(i)-64; //posição da letra no alfabeto
-    var deslocamentoDaLetra = (posicionamentoDaLetra + parseInt(chaveCodificar)) % 26; //deslocamento da letra para cifra de cesar e se mantem dentro das 26 letras
-    resultado += array[deslocamentoDaLetra - 1];
+  for (let i = 0; i < textoCodificar.length; i++) {
+    for (let v = 0; v < array.length; v++){
+      if(textoCodificar[i]==array[v]){
+          let posicao = array.indexOf(array[v]) + chaveCodificar;
+          resultado[v] =  array[posicao];
+          console.log(resultado[v]);
+      }
+    }
   }
-   document.getElementById("resultado").innerHTML = resultado;
+   // document.getElementById("resultado").innerHTML = resultado;
 });
 
 
-// document.getElementById("Decodificar").addEventListener('click', () => {
-//   var chaveDecodificar = document.querySelector("#chaveDecodificar").value;
-//   var textoDecodificar = document.getElementById("textoDecodificar").value.toUpperCase();
-//   var resultado = "";
-//
-//   for (var i = 0; i < textoDecodificar.length; i++) {
-//     var posicionamentoDaLetra = textoDecodificar.charCodeAt(i) - 64;
-//     var deslocamentoDaLetra = (posicionamentoDaLetra - parseInt(chaveDecodificar)) % 26;
-//     resultado += array[deslocamentoDaLetra - 1];
-//   }
-//   document.getElementById("resultado").innerHTML = resultado;
-// });
+document.getElementById("Decodificar").addEventListener('click', () => {
+  let chaveDecodificar = parseInt(document.querySelector("#chaveDecodificar").value);
+  let textoDecodificar = document.getElementById("textoDecodificar").value.toLowerCase();
+  let resultado = [];
+
+  for (let i = 0; i < textoDecodificar.length; i++) {
+    for (let v = 0; v < array.length; v++){
+      if(textoDecodificar[i]==array[v]){
+          let posicao = array.indexOf(array[v]) - chaveDecodificar;
+          resultado[v] =  array[posicao];
+          console.log(resultado[v]);
+      }
+    }
+  }
+   document.getElementById("resultado").innerHTML = resultado.join(".");
+});
