2babdec8188a99eab032a52ecb3e8e05da6cb348
isadorabnunes <isadora.nunes@novaandradina.org>
2019-04-09 14:38:55 -0300

    atualizando again

--- js/cesar.js
+++ js/cesar.js
@@ -3,34 +3,50 @@ const array = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 document.getElementById("codificar").addEventListener('click', () => {
   let chaveCodificar = parseInt(document.querySelector("#chaveCodificar").value);
   let textoCodificar = document.getElementById("textoCodificar").value.toLowerCase();
-  let resultado = [];
+  let resultado = " ";
 
   for (let i = 0; i < textoCodificar.length; i++) {
     for (let v = 0; v < array.length; v++){
-      if(textoCodificar[i]==array[v]){
+      if(textoCodificar.charAt(i)==array[v]){
           let posicao = array.indexOf(array[v]) + chaveCodificar;
-          resultado[v] =  array[posicao];
-          console.log(resultado[v]);
+          if(posicao > 25){
+            let tratamento = posicao - 26;
+            resultado +=  array[tratamento];
+          }else{
+            resultado +=  array[posicao];
+            console.log(resultado[v]);
+          }
+      }
+      if(textoCodificar.charAt(i) == " "){
+        resultado += " ";
       }
     }
   }
-   // document.getElementById("resultado").innerHTML = resultado;
+   document.getElementById("resultado").innerHTML = resultado;
 });
 
 
-document.getElementById("Decodificar").addEventListener('click', () => {
+document.getElementById("decodificar").addEventListener('click', () => {
   let chaveDecodificar = parseInt(document.querySelector("#chaveDecodificar").value);
   let textoDecodificar = document.getElementById("textoDecodificar").value.toLowerCase();
-  let resultado = [];
+  let resultado = " ";
 
   for (let i = 0; i < textoDecodificar.length; i++) {
     for (let v = 0; v < array.length; v++){
-      if(textoDecodificar[i]==array[v]){
+      if(textoDecodificar.charAt(i)==array[v]){
           let posicao = array.indexOf(array[v]) - chaveDecodificar;
-          resultado[v] =  array[posicao];
-          console.log(resultado[v]);
+          if(posicao < 0 ){
+            let tratamento = posicao + 26;
+            resultado +=  array[tratamento];
+          }else{
+            resultado +=  array[posicao];
+            console.log(resultado[v]);
+          }
+      }
+      if(textoDecodificar.charAt(i) == " "){
+        resultado += " ";
       }
     }
   }
-   document.getElementById("resultado").innerHTML = resultado.join(".");
+   document.getElementById("resultado").innerHTML = resultado;
 });
